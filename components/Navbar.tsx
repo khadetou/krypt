e38be1a27f0be8d33200d2278c598b9ff57db2cb
khadetou <khadetou96@gmail.com>
2021-12-31 01:15:27 +0000

    navbar logo setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import { HiMenuAlt4 } from 'react-icons/hi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+
 const Navbar = () => {
     return (
-        <div>
-            Navbar
-        </div>
+        <nav className='w-full flex md:justify-center justify-between items-center text-white'>
+            <div className='md:flex-[0.5] flex-initial justify-items-center'>
+                <div className="w-32 cursor-pointer">
+                    <Image src="/images/logo.png" alt="logo" width="369" height="94" />
+                </div>
+            </div>
+        </nav>
     )
 }
 
